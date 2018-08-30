50005d7d98e74478872473e2680e4136ddca7694
sandergl <sandergl@rose-hulman.edu>
2018-08-30 15:55:50 -0400

    me

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -73,7 +73,7 @@ print(3607*34227)
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -87,7 +87,7 @@ print(3607*34227)
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).  Note that
 #   this file (m2_todo_and_commit_push.py) is now displayed in BLUE.
 #
